07c874880aa498dd63048ecbaa41a297520d37ca
Sam Phillips <samdphillips@gmail.com>
2021-11-16 15:32:59 -0800

    [class] add to CI

---
+++ rhombus-examples-class-interop/README.md
@@ -0,0 +1,5 @@
+# rhombus-examples-class-interop
+
+<!> Warning this example requires a patched version of the Rhombus prototype.
+
+https://github.com/samdphillips/shrubbery-rhombus-0/tree/racket-class
---
+++ rhombus-examples-class-interop/info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define name "rhombus-examples-class-interop")
+(define collection "rhombus-examples")
+(define deps
+  '("base"
+    "https://github.com/samdphillips/shrubbery-rhombus-0.git#racket-class"))
+(define module-suffixes '(#"rhm"))
+
