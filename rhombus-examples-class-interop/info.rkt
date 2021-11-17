6614cddb77f891c54acd541ee9eea40c16941fa6
Sam Phillips <samdphillips@gmail.com>
2021-11-16 17:15:54 -0800

    [class] add missing dep

--- rhombus-examples-class-interop/info.rkt
+++ rhombus-examples-class-interop/info.rkt
@@ -4,6 +4,7 @@
 (define collection "rhombus-examples")
 (define deps
   '("base"
+    "gui-lib"
     "https://github.com/samdphillips/shrubbery-rhombus-0.git#racket-class"))
 (define module-suffixes '(#"rhm"))
 
